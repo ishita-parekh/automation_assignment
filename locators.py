d902f08f636c8664b18145409040d20e048b8ccd
Ishita Parekh <ishitaparekh78@gmail.com>
2021-08-10 20:19:59 +0530

    Add files via upload
    This is a locators.py file. This file contains the elements on signup, sign in and work order web pages required for test scripts.

---
+++ locators.py
@@ -0,0 +1,36 @@
+# Signup Page locator elements
+signup_email = "signup_username"
+signup_password = "signup_password"
+signup_firstname = "signup_firstname"
+signup_lastname = "signup_lastname"
+signup_phoneno = "signup_companyPhone"
+signup_companyname = "signup_companyname"
+signup_title = "signup_jobTitleLabel"
+signup_btn = "//button[@analytics-category='Signup']"
+
+# Login Page locator elements
+email = "username"
+password = "password"
+login_btn = "form[name='loginForm'] > .ng-scope"
+
+# Landing Page
+landing_page_url = "https://app.onupkeep.com/web/work-orders"
+
+# Workorder page locator elements
+workorder_create_btn = ".dJSzjo.erEbDM.styled__AutoHeightButton-sc-18kdwn2-16.styled__Wrapper-sc-6m8pxr-0"
+workorder_create_title = "//input[@class='_test-field-title']"
+workorder_create_desc = "//textarea[@class='_test-field-description']"
+workorder_create_duration = "//input[@class='_test-field-duration']"
+workorder_create_image = "//label[@for='file-upload-input']"
+workorder_create_priority = ".fPqTDi.styled__Wrapper-sc-6sc1ip-0 > div:nth-of-type(3) > .radio-option > .option-label"
+workorder_create_category = ".css-1e8w2ey-control.react-select__control > .css-1hwfws3.react-select__value-container.react-select__value-container--has-value"
+workorder_create_submit = ".hrkaCH.styled__Wrapper-sc-6m8pxr-0"
+workorder_list_search = ".search-form > .dJSzjo.gsTijM.ireEvJ.styled__SubmitButton-s2rtbi-0.styled__Wrapper-rjpgx3-0.styled__Wrapper-sc-6m8pxr-0"
+workorder_menu = "//body/div[@id='app']/div[1]/div[2]/ul[1]/div[1]/li[1]/a[1]/div[1]"
+searched_result = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/span[1]/span[1]/span[1]"
+bookmark_filter = "div#bookmark-filter  .hCdFoq.styled__FilterLabel-ww3q2d-4"
+more_filter = ".gPXafa.styled__FunnelIcon-ww3q2d-9"
+more_filter_opt = "//span[contains(text(),'Repeating Only')]"
+filter_show_results = "//button[contains(text(),'Show Results')]"
+reset_filter = "//body/div[@id='app']/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/div[8]/div[1]/div[2]/span[1]/*[1]"
+workorder_search = "search"
